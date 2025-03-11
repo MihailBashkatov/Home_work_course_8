94e12f947b09a32acf19c87d324ebf68938bd964
mihail <mihail.bashkatov@gmail.com>
2025-03-11 18:24:23 +0100

    Creating models Course and Lesson in materials/models.py

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    """ Registering model Course """
+    name = models.CharField(
+        max_length=300,
+        verbose_name="Name",
+    )
+
+    description = models.TextField(
+        verbose_name="Description"
+    )
+
+    preview = models.ImageField(
+        upload_to="materials/course/preview/%Y/%m/%d/",
+        default=None,
+        null=True,
+        blank=True,
+        verbose_name="Saved course preview",
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+        ordering = ["name",]
+
+
+class Lesson(models.Model):
+    """ Registering model Lesson """
+    name = models.CharField(
+        max_length=300,
+        verbose_name="Name",
+    )
+
+    description = models.TextField(
+        verbose_name="Description"
+    )
+
+    preview = models.ImageField(
+        upload_to="materials/lessons/preview/%Y/%m/%d/",
+        default=None,
+        null=True,
+        blank=True,
+        verbose_name="Saved course preview",
+    )
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, related_name="lessons")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Lesson"
+        verbose_name_plural = "Lessons"
+        ordering = ["name",]
