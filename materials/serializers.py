655a1db3f33f661b0d06cc251b5f4c8e3411a47f
mihail <mihail.bashkatov@gmail.com>
2025-03-11 23:24:39 +0100

    Correcting LessonSeriazer

--- materials/serializers.py
+++ materials/serializers.py
@@ -7,11 +7,11 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description']
+        fields = ['id', 'name', 'description']
 
 
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
-        fields = ['id', 'title', 'description']
+        fields = ['id', 'name', 'description', 'course']
