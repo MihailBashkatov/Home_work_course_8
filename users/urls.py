807d4781fa95bf417bb0bb40b3f4e2613698a818
mihail <mihail.bashkatov@gmail.com>
2025-03-11 23:50:38 +0100

    Adding Serializer, APIViews for User in app user

---
+++ users/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import UserCreateAPIView, UserListAPIView, UserRetreiveAPIView, UserUpdateAPIView, UserDestroyAPIView
+
+app_name = UsersConfig.name
+
+
+urlpatterns = [
+    path("user/create/", UserCreateAPIView.as_view(), name="user_create"),
+    path("users/", UserListAPIView.as_view(), name="user_list"),
+    path("user/<int:pk>/", UserRetreiveAPIView.as_view(), name="user_detail"),
+    path(
+        "user/update/<int:pk>/", UserUpdateAPIView.as_view(), name="user_update"
+    ),
+    path(
+        "user/delete/<int:pk>/", UserDestroyAPIView.as_view(), name="user_delete"
+    ),
+]
