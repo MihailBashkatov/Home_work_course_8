7819d25ff7dfedb538dc02f16db0061ba54a3f66
mihail <mihail.bashkatov@gmail.com>
2025-03-13 21:37:01 +0100

    Adding logic to count lessons per course in CourseSerializer in materials/serializers.py

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,10 +4,16 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons = serializers.SerializerMethodField()
+    #
+    @staticmethod
+    def get_lessons(course):
+        """ Getting amount of lessons per course"""
+        return course.lessons.all().count()
 
     class Meta:
         model = Course
-        fields = ["id", "name", "description"]
+        fields = ["id", "name", "description", "lessons"]
 
 
 class LessonSerializer(serializers.ModelSerializer):
