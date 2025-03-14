fd02fdde47b8c28450b49aa4d0592f54a5a97f72
mihail <mihail.bashkatov@gmail.com>
2025-03-14 16:14:42 +0100

    Adding field lessons to CourseSerializer in materials/serializers.py

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,21 +3,24 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lessons = serializers.SerializerMethodField()
-    #
-    @staticmethod
-    def get_lessons(course):
-        """ Getting amount of lessons per course"""
-        return course.lessons.all().count()
-
-    class Meta:
-        model = Course
-        fields = ["id", "name", "description", "lessons"]
-
-
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
         fields = ["id", "name", "description", "course"]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
+
+    #
+    @staticmethod
+    def get_lessons_count(course):
+        # print(course.lessons.all())
+        """Getting amount of lessons per course"""
+        return course.lessons.all().count()
+
+    class Meta:
+        model = Course
+        fields = ["id", "name", "description", "lessons_count", "lessons"]
