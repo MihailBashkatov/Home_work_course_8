1332a663d3c74c4fcf210896285743c17750034e
mihail <mihail.bashkatov@gmail.com>
2025-03-14 17:43:22 +0100

    Adding PaymentSerializer in users/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from materials.models import Course, Lesson
-from users.models import User
+from users.models import User, Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,3 +9,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = ["user", "payment_date", "payment_summ", "payment_mode", "paid_course", "paid_lesson"]
+
