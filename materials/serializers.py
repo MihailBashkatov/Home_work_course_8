81340758baec2bc0229cf2fdcb2bb1de6dbdb2aa
mihail <mihail.bashkatov@gmail.com>
2025-03-18 19:04:26 +0100

    Correcting field lessons of CourseSerializer to read_only=True

--- materials/serializers.py
+++ materials/serializers.py
@@ -12,7 +12,7 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(many=True)
+    lessons = LessonSerializer(many=True, read_only=True)
 
     #
     @staticmethod
