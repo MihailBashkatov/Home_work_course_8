d4a754952f83fdb83a6f86c139a274fa22bd3d2d
mihail <mihail.bashkatov@gmail.com>
2025-03-18 19:11:23 +0100

    Adding configuration DEFAULT_AUTHENTICATION_CLASSES in config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
