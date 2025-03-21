04fa84a1df02895996991553457f03f49c19ba3c
mihail <mihail.bashkatov@gmail.com>
2025-03-21 22:34:32 +0100

    Adding IsOwner permission in users/permissions.py

--- users/permissions.py
+++ users/permissions.py
@@ -1,7 +1,20 @@
 from rest_framework import permissions
 
 class ModeratorAccessPermission(permissions.BasePermission):
-    message = 'Adding permissions for moderatorsd.'
+    """
+    Check if user is in Group Moderators
+    """
+    message = 'Adding permissions for moderators.'
 
     def has_permission(self, request, view):
         return request.user.groups.filter(name='Moderators').exists()
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+
+        return obj.owner == request.user
