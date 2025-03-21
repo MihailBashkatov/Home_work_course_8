c45c0cae16c877216c7bab8693cb94b1cbc0d460
mihail <mihail.bashkatov@gmail.com>
2025-03-21 22:31:59 +0100

    Correcting fields value in users/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -8,7 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "email"]
+        fields = ['email', 'password']
+
 
 
 class PaymentSerializer(serializers.ModelSerializer):
