92e89d351ab0a85979bd8ab24a422ebb58b52a4a
mihail <mihail.bashkatov@gmail.com>
2025-03-22 15:03:06 +0100

    Some correcting in users/permissions.py

--- users/permissions.py
+++ users/permissions.py
@@ -19,4 +19,8 @@ class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-        return obj.owner == request.user
+        if obj.owner == request.user:
+            return True
+        return False
+
+
